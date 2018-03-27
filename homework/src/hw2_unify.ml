ef9ad23579e8bbe56c40ecf3eed0c0b8728aeabd
egormkn <egormkn@yandex.ru>
2018-03-27 04:12:29 +0300

    Finished hw2_unify

--- homework/src/hw2_unify.ml
+++ homework/src/hw2_unify.ml
@@ -1,121 +1,79 @@
-(* Unification algorithm *)
+(** The unification problem in first-order logic can be expressed as follows:
+    Given two terms containing some variables, find, if it exists, the simplest
+    substitution (i.e., an assignment of some term to every variable) which
+    makes the two terms equal. The resulting substitution is called
+    the most general unifier .
 
+    @author Egor Makerenko
+*)
+
+(** Algebraic term is a variable or a function of some variables *)
 type algebraic_term =
   | Var of string
   | Fun of string * (algebraic_term list);;
 
-type substitution = (string * algebraic_term) list
-type equation = algebraic_term * algebraic_term
-type system_of_equations = equation list
-
-(* Unique function name generator *)
+(** Unique function name generator *)
 let unique_name = Stream.from (fun i -> Some ("temp_" ^ string_of_int i));;
 
-(* Returns one equation build from a list of equations *)
+(** Returns one equation equivalent to given system of equations *)
 let system_to_equation system =
   let new_name = Stream.next unique_name in
-  let (l, r) = List.split system in
-  (Fun (new_name, l), Fun (new_name, r));;
+  let (left, right) = List.split system in
+  (Fun (new_name, left), Fun (new_name, right));;
 
-(* Applies substitution to algebraic term *)
-let apply_substitution substs_list term =
-  let rec apply (key, subst) term =
+(** Returns a term after applying given substitution *)
+let apply_substitution substitution_list term =
+  let rec apply substitution term =
+    let (key, new_term) = substitution in
     match term with
-    | Var v -> if (key = v) then subst else term
-    | Fun (f, t) -> Fun (f, List.map (apply (key, subst)) t)
+    | Var v -> if (key = v) then new_term else term
+    | Fun (name, args) -> Fun (name, List.map (apply substitution) args)
   in
-  List.fold_right apply substs_list term;;
+  List.fold_right apply substitution_list term;;
 
-let check_solution substs_list system =
+(** Returns True if a substitution is a solution of a given system *)
+let check_solution substitution_list system =
   let (l, r) = system_to_equation system in
-  let substitute = apply_substitution substs_list in
+  let substitute = apply_substitution substitution_list in
   substitute l = substitute r;;
-
-let solve_system x = failwith "Not implemented";;
-
 
 exception NoSolution of string;;
 module StringSet = Set.Make (String);;
 
-let rec contains str at msk =
-  match at with
-    (Var a) -> if a = str then msk lor 1 else msk
-  | (Fun (f, l)) -> (contains_l str l msk) lor (if str = f then 2 else 0)
-and contains_l str l msk =
-  match l with
-    [] -> msk
-  | (h::t) -> (contains str h msk) lor (contains_l str t msk);;
-
-let rec algebraic_term_to_string (at : algebraic_term) =
-  let rec impl a =
-    match a with
-    | Var x -> x
-    | Fun(f, l) -> f ^ "(" ^ impl_for_list l ^ ")"
-
-  and impl_for_list lt =
-    match lt with
-    | [] -> ""
-    | (h::[]) -> impl h
-    | (h::t) -> (impl h) ^ " " ^ (impl_for_list t)
+(** Returns a solution of a given system or None, if there is no solution *)
+let solve_system system =
+  let error message = raise (NoSolution message) in
+  let rec contains variable term =
+    match term with
+    | Var v -> v = variable
+    | Fun (f, args) -> List.exists (contains variable) args
   in
-  impl at;;
-
-let memv str at =
-  contains str at 0 land 1 <> 0;;
-
-let rec subst (s : algebraic_term) (x : string) (t : algebraic_term) : algebraic_term =
-  match t with
-  | Var y -> if (x = y) then s else t
-  | Fun(f, l) -> Fun(f, List.map (subst s x) l);;
-
-(* Major implementations: system_to_equation, apply_substitution,    *)
-(* check_solution, solve_system.                                     *)
-let apply_substitution_sys subst sys =
-  let rec impl subst sys acc =
-    match sys with
-      [] -> List.rev acc
-    | ((l, r)::t) -> impl subst t (((apply_substitution subst l), (apply_substitution subst r))::acc) in
-  impl subst sys [];;
-
-
-let rec solve_system (sys : system_of_equations) : substitution option =
-  let get_args_sys l1 l2 =
-    let rec impl l1 l2 ans =
-      match (l1, l2) with
-        ([], []) -> List.rev ans
-      | (h1::t1, h2::t2) -> impl t1 t2 ((h1, h2)::ans)
-      | _ -> failwith "never" in
-    impl l1 l2 [] in
-
-  let rec impl sys resolved =
-    if StringSet.cardinal resolved = List.length sys then sys else
-      match sys with
-        [] -> raise (NoSolution "Empty system")
-      | (lhs, rhs)::tail ->
-        let cur = lhs, rhs in
-        if lhs = rhs then impl tail resolved else
-          match (lhs, rhs) with
-            Var a, any -> if memv a any then raise (NoSolution "Fourth rule abused")
-            else let resolved = StringSet.add a resolved in
-              impl (List.append (apply_substitution_sys [a, any] tail) [cur]) resolved
-          | any, Var a -> impl (List.append tail [rhs, lhs]) resolved
-          | Fun(f, l1), Fun(g, l2) -> if f <> g || List.length l1 <> List.length l2 then raise (NoSolution "Third rule abused")
-            else impl (List.append tail (get_args_sys l1 l2)) resolved in
-
-  let dewrap sys =
-    let rec impl sys ans =
-      match sys with
-        [] -> List.rev ans
-      | ((Var a, rhs)::tail) ->
-        impl tail ((a, rhs)::ans)
-      | _ -> failwith "it's impossible, sorry" in
-    impl sys [] in
-
+  let rec solve system resolved =
+    match system with
+    | [] -> error "System is empty"
+    | _ when StringSet.cardinal resolved = List.length system -> system
+    | equation :: tail ->
+      match equation with
+      | (l, r) when l = r -> solve tail resolved
+      | (Var v, term) when contains v term -> error "Error of type x = f(x)"
+      | (Var v, term) ->
+        let resolved = StringSet.add v resolved in
+        let substitute = apply_substitution [(v, term)] in
+        let tail = List.map (fun (l, r) -> (substitute l, substitute r)) tail in
+        solve (tail @ [equation]) resolved
+      | (term, Var v) -> solve (tail @ [(Var v, term)]) resolved
+      | (Fun (f, l1), Fun (g, l2)) when f = g && List.length l1 = List.length l2 ->
+        let new_equations = List.combine l1 l2 in
+        solve (tail @ new_equations) resolved
+      | (Fun _, Fun _) -> error "Third rule abused"
+  in
+  let substitution_of_equation (left, right) =
+    match left with
+    | Var v -> (v, right)
+    | _ -> failwith "Equation is not in form x = T"
+  in
   try
-    let resolved_system = impl sys StringSet.empty in
-    print_string "";
-    (Some (dewrap resolved_system))
-
-  with (NoSolution msg) ->
-    print_string "";
-    None;;
+    let resolved_system = solve system StringSet.empty in
+    let solution = List.map substitution_of_equation resolved_system in
+    Some solution
+  with NoSolution message -> None;;
--- homework/src/hw2_unify.mli
+++ homework/src/hw2_unify.mli
@@ -1,15 +1,31 @@
+(** The unification problem in first-order logic can be expressed as follows:
+    Given two terms containing some variables, find, if it exists, the simplest
+    substitution (i.e., an assignment of some term to every variable) which
+    makes the two terms equal. The resulting substitution is called
+    the most general unifier .
+
+    @author Egor Makerenko
+*)
+
+(** Algebraic term is a variable or a function of some variables *)
 type algebraic_term =
   | Var of string
   | Fun of string * (algebraic_term list)
 
-(* По списку уравнений вернуть одно уравнение *)
+(* These types might also be used for convenience:
+
+   type substitution = string * algebraic_term
+   type equation = algebraic_term * algebraic_term
+   type system_of_equations = equation list *)
+
+(** Returns one equation equivalent to given system of equations *)
 val system_to_equation: (algebraic_term * algebraic_term) list -> (algebraic_term * algebraic_term)
 
-(* Применить подстановку к уравнению *)
+(** Returns a term after applying given substitution *)
 val apply_substitution: (string * algebraic_term) list -> algebraic_term -> algebraic_term
 
-(* Проверить решение *)
+(** Returns True if a substitution is a solution of a given system *)
 val check_solution: (string * algebraic_term) list -> (algebraic_term * algebraic_term) list -> bool
 
-(* Решить систему; если решения нет -- вернуть None *)
+(** Returns a solution of a given system or None, if there is no solution *)
 val solve_system: (algebraic_term * algebraic_term) list -> (string * algebraic_term) list option
